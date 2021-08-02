967b641e61cf8f0fe5e7cff22074c43816ae9854
chantellapierre <66752117+chantellapierre@users.noreply.github.com>
2021-08-02 15:38:58 -0700

    Part 3

---
+++ part3/App.js
@@ -0,0 +1,10 @@
+function App() {
+	return (
+		<div>
+			<Person name="Jeff" age={21} hobbies={[ 'sitting', 'on the', 'couch' ]} />
+			<Person name="Geoff" age={31} hobbies={[ 'walking', 'to the', 'fridge' ]} />
+			<Person name="Jeffreigh" age={41} hobbies={[ 'extreme ironing', 'frivolous lawsuits' ]} />
+			<Person name="Jefferson" age={51} hobbies={[ 'drinking beer', 'do you like beer senator?' ]} />
+		</div>
+	);
+}
